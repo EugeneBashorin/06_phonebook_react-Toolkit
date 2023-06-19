c1338c4655bf048ab1989a96f2aaae7a1702bce2
EugeneBashorin <bashorineugene@gmail.com>
2023-06-19 12:49:47 +0300

    added lib redux-persist to keep,init,save data in storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./userSlice";
 import { favoriteFilterReducer } from "./favoriteFilterSlice";
 import { userFilterReducer } from "./userFilterSlice";
+import storage from "redux-persist/lib/storage";
+import {persistReducer, persistStore} from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    favoriteFilter: favoriteFilterReducer,
+    userFilter: userFilterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) 
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        favoriteFilter: favoriteFilterReducer,
-        userFilter: userFilterReducer,
-    }
+    reducer: persistedReducer,
+    // reducer: {
+    //     user: userReducer,
+    //     favoriteFilter: favoriteFilterReducer,
+    //     userFilter: userFilterReducer,
+    // }
+    middleware: [thunk]
 });
+
+export const persistor = persistStore(store);
